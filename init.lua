69c55c59d579cbb99f052f169efa983a4e8f578b
rikcav <henriquearau000@gmail.com>
2024-03-30 13:33:08 -0300

    feat: the start point

---
+++ init.lua
@@ -0,0 +1,13 @@
+require("comments")
+require("keymaps")
+require("lsp")
+require("lualine").setup()
+require("null_ls")
+require("nvim-tree").setup()
+require("options")
+require("plugins")
+require("treesitter")
+require("gitsigns").setup()
+require("doom_one").config()
+-- require("rest")
+require("telescope").load_extension "file_browser"

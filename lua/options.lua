f3690f5e998cf52f6047de2eb7b53e15036ae78f
rikcav <henriquearau000@gmail.com>
2024-03-30 13:30:33 -0300

    feat: my preffered options

---
+++ lua/options.lua
@@ -0,0 +1,63 @@
+vim.opt.mouse = "a"
+
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 4
+
+vim.opt.cmdheight = 1
+
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
+
+vim.opt.foldmethod="indent"
+vim.opt.foldlevel=99
+
+vim.opt.cursorline = true
+
+vim.opt.wrap = false
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.showtabline = 2
+vim.opt.expandtab = true
+
+vim.opt.shiftwidth = 2
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.completeopt = { "menu","menuone","noselect","noinsert" }
+
+vim.opt.fileformat = "unix"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 4
+
+vim.opt.backup = false
+vim.opt.swapfile = false
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.conceallevel = 0
+
+vim.opt.undofile = false
+vim.opt.writebackup = false
+
+vim.opt.timeoutlen = 1000
+vim.opt.updatetime = 300
+
+vim.opt.pumheight = 10
+vim.opt.showmode = false
+
+vim.opt.guifont = "monospace:h17"
+vim.opt.termguicolors = true
+
+vim.opt.shortmess:append "c"

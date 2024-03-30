8a6bee816b03d676edc5c512d8edcde3545c13c8
rikcav <henriquearau000@gmail.com>
2024-03-30 13:29:59 -0300

    feat: treesitter config

---
+++ lua/treesitter.lua
@@ -0,0 +1,17 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
+
+configs.setup {
+  sync_install = false,
+  ignore_install = { "" },
+  auto_install = true,
+  highlight = {
+    enable = true,
+    disable = { "" },
+    additional_vim_regex_highlighting = false,
+  },
+  indent = true,
+  enable_autocmd = false,
+}
